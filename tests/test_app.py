2794d8d065648324eb8e91307cb05307858e96a5
ATTY Lionel <latty@unowhy.com>
2024-08-24 10:10:49 +0200

    ci: tox - add scripts/endpoints invokation for testing + utests - disable catching exceptions when invokating cli_runner
    Signed-off-by: ATTY Lionel <latty@unowhy.com>

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import re
 
 import pytest
+import requests
 from rich._emoji_codes import EMOJI
 
 from cruft_helloworld import __project_name__ as application_name
@@ -9,7 +10,7 @@ from cruft_helloworld.app import cli, hello_world
 
 
 def test_app_cli_option_version(cli_runner):
-    result = cli_runner.invoke(cli, ["--version"])
+    result = cli_runner.invoke(cli, ["--version"], catch_exceptions=False)
     assert result.exit_code == 0
     application_name_expected = application_name
     application_version_expected = application_version
@@ -21,25 +22,33 @@ def test_app_cli_option_version(cli_runner):
 
 # @pytest.mark.skip
 def test_app_cli_option_show_banner(cli_runner):
-    result = cli_runner.invoke(cli, ["--show-banner"])
+    result = cli_runner.invoke(cli, ["--show-banner"], catch_exceptions=False)
     assert result.exit_code == 0
     version_message = f"{application_name}, version {application_version}"
     assert len(result.output) > len(version_message)
     assert result.output.count("\n") > 1
 
 
-def test_app_cli_option_verbose(cli_runner):
-    result = cli_runner.invoke(cli, ["--verbose"])
+def test_app_cli_option_verbose(cli_runner, mocker):
+    # no need to use 'internet' for testing "verbose" option
+    # we can raise an exception at the first internet request
+    # and follow the default running path if no internet available
+    mocker.patch(
+        "cruft_helloworld.services.globe_emoji_with_geoip.requests.get",
+        side_effect=requests.exceptions.RequestException,
+    )
+
+    result = cli_runner.invoke(cli, ["--verbose"], catch_exceptions=False)
     assert result.exit_code == 0
     assert "DEBUG" not in result.output
 
-    result = cli_runner.invoke(cli, ["-vv"])
+    result = cli_runner.invoke(cli, ["-vv"], catch_exceptions=False)
     assert result.exit_code == 0
     assert "DEBUG" in result.output
 
 
 def test_app_cli_help(cli_runner):
-    result = cli_runner.invoke(hello_world, ["--help"])
+    result = cli_runner.invoke(hello_world, ["--help"], catch_exceptions=False)
     assert result.exit_code == 0
     assert (
         "--globe-emoji [EUROPE_AFRICA|ASIA_AUSTRALIA|AMERICAS|WITH_MERIDIANS]"
@@ -60,7 +69,9 @@ def test_app_cli_hello_world(
     cli_runner, input_globe_emoji_name: str, expected_globe_emoji_char: str
 ):
     # https://github.com/willmcgugan/rich/blob/a3f5609202e9aa45751ce9baa3a72462ed1cc488/tests/test_console.py#L192
-    result = cli_runner.invoke(hello_world, ["--globe-emoji", input_globe_emoji_name])
+    result = cli_runner.invoke(
+        hello_world, ["--globe-emoji", input_globe_emoji_name], catch_exceptions=False
+    )
     assert result.exit_code == 0
     assert result.output == f"Hello {expected_globe_emoji_char}\n"
 
@@ -69,7 +80,9 @@ def test_error_app_cli_hello_world(cli_runner):
     click_option_name = "globe-emoji"
     wrong_emoji_name = "no-existing-click-option"
     result = cli_runner.invoke(
-        hello_world, [f"--{click_option_name}", wrong_emoji_name]
+        hello_world,
+        [f"--{click_option_name}", wrong_emoji_name],
+        catch_exceptions=False,
     )
     assert result.exit_code == 2
     assert (
@@ -80,7 +93,7 @@ def test_error_app_cli_hello_world(cli_runner):
 
 @pytest.mark.use_internet
 def test_app_cli_hello_world_without_option(cli_runner):
-    result = cli_runner.invoke(hello_world)
+    result = cli_runner.invoke(hello_world, catch_exceptions=False)
     assert result.exit_code == 0
     hello_world_result = result.output
     regex = r"Hello (?P<globe_emoji>.)"
